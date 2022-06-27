35162f25fdc29934db745856e14049d7df466e00
SetsunaTsuyuri <preciousprite@gmail.com>
2022-06-27 19:09:50 +0900

    構成変更

---
+++ Assets/Kawasaki/Scripts/ScriptTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kawasaki
+{
+    /// <summary>
+    /// スクリプト作成テスト
+    /// </summary>
+    public class ScriptTest : MonoBehaviour
+    {
+    }
+}
