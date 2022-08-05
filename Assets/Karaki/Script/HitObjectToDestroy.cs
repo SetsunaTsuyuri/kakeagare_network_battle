b380a44bbdcc1a5b427f7a8f503186c4e7688ddd
K-Koya <action.gamer.k@gmail.com>
2022-08-05 17:05:20 +0900

    消去方法について、PhotonのDestoryに対応

--- Assets/Karaki/Script/HitObjectToDestroy.cs
+++ Assets/Karaki/Script/HitObjectToDestroy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 namespace Karaki
 {
@@ -12,6 +13,12 @@ namespace Karaki
         [SerializeField, Tooltip("この回数オブジェクトが接触すると消させる")]
         int _hitCount = 1;
 
+        /// <summary>true : 自分のシステムで作られたもの</summary>
+        bool _isMyCreated = false;
+
+        /// <summary>true : 自分のシステムで作られたもの</summary>
+        public bool IsMyCreated { set => _isMyCreated = value; }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.CompareTag(_tagNameObject))
@@ -22,10 +29,13 @@ namespace Karaki
 
         void OnHit()
         {
-            _hitCount--;
-            if (_hitCount < 1)
+            if (_isMyCreated)
             {
-                Destroy(gameObject);
+                _hitCount--;
+                if (_hitCount < 1)
+                {
+                    PhotonNetwork.Destroy(this.gameObject);
+                }
             }
         }
     }
