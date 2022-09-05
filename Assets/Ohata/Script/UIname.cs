257d9b79438f3b845ea874764bd85fe9ebf53ac7
Kaito913ps <86725129+Kaito913ps@users.noreply.github.com>
2022-09-05 19:32:00 +0900

    PlayerやEnemyの名前や矢印を表示するようにする

--- Assets/Ohata/Script/UIname.cs
+++ Assets/Ohata/Script/UIname.cs
@@ -7,7 +7,7 @@ using Kawasaki;
 
 namespace Ohata
 {
-    public class UIname : MonoBehaviour
+    public class UIName : MonoBehaviour
     {
         [SerializeField, Header("playerの名前")]
         TextMeshProUGUI _text;
@@ -17,21 +17,67 @@ namespace Ohata
         [SerializeField, Header("テキスト")]
         PlayerUI _playerUi;
 
+        Camera _camera;
+
+        RectTransform _rectTransform;
+
         PhotonView _viewName;
         void Start()
         {
-            if(_playerUi == PlayerUI.You)
-            {
-                Player myPlayer = PlayersManager.Current.GetMyPlayer();
-                _text.text = "You";
-                _target = myPlayer.transform;
-            }
-            else if(_playerUi == PlayerUI.Enemy)
-            {
-                _text.text = "Enemy";
-                Player enemy = PlayersManager.Current.GetAnotherPlayer();
-                _target = enemy.transform;
+            _rectTransform = transform as RectTransform;
+            _camera = Camera.main;
+            _text = GetComponentInChildren<TextMeshProUGUI>();
 
+           
+        }
+
+        
+
+        private void LateUpdate()
+        {
+            if(_target == null)
+            {
+                RegisterPlayer();
+            }
+            else
+            {
+                Vector2 position = RectTransformUtility.WorldToScreenPoint(_camera, _target.position);
+                _rectTransform.position = position;
+            }
+           
+           
+
+             
+        }
+
+
+
+        private void RegisterPlayer()
+        {
+            switch (_playerUi)
+            {
+                case PlayerUI.You:
+
+                    Player myPlayer = PlayersManager.Current.GetMyPlayer();
+                    if(myPlayer != null)
+                    {
+                        _text.text = "You";
+                        _target = myPlayer.transform;
+                    }
+                   
+                    break;
+                case PlayerUI.Enemy:
+                  
+                    Player enemy = PlayersManager.Current.GetAnotherPlayer();
+                    if(enemy != null)
+                    {
+                        _text.text = "Enemy";
+                        _target = enemy.transform;
+                    }
+                   
+                    break;
+                default:
+                    break;
             }
         }
     }
