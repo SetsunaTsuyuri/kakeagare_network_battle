8d419f507ac10289dc0379a8665cf62dc196fe4a
K-Koya <action.gamer.k@gmail.com>
2022-10-03 12:22:50 +0900

    プレイヤーの動きを制限するブロックを個々のプレイヤーごとに生成・削除するコンポーネントを作成

---
+++ Assets/Karaki/Script/StartingBlock.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingBlock : MonoBehaviour
+{
+    [SerializeField, Tooltip("ゲームが開始するまで特定範囲から出られなくするブロックのプレハブ")]
+    GameObject _blockPref = null;
+
+    [SerializeField, Tooltip("スポーン地点の位置")]
+    Transform _SpawnPoint = null;
+
+    [SerializeField, Tooltip("スポーン地点からどのくらい上に配置するかを指定")]
+    float _offsetFromSpawnPosition = 4f;
+
+    /// <summary>生成したブロック実体</summary>
+    GameObject _block = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _block = Instantiate(_blockPref, _SpawnPoint.position + Vector3.up * _offsetFromSpawnPosition, _SpawnPoint.rotation);
+    }
+
+    /// <summary>スタートブロックを消す</summary>
+    public void DestroyBlock()
+    {
+        Destroy(_block);
+    }
+}
